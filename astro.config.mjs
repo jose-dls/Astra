e780dd94b1253d4dd5899b6b0c770b49d2e52b15
J. De Los Santos <163740390+jose-dls@users.noreply.github.com>
2025-04-27 02:05:25 +0930

    Add Better Dark Mode Support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), react()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
