77513de541eb99673c63d67e19132c8522235874
J. De Los Santos <163740390+jose-dls@users.noreply.github.com>
2025-04-30 19:22:48 +0930

    Added Astra Branding

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'ASTRA';
-export const SITE_DESCRIPTION = 'Astra: A modern !';
+export const SITE_DESCRIPTION = 'Astra: A modern blog template.';
